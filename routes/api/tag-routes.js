8ba011aae3b81d40b8107aa29ad9677685c5b975
SJRXD <rohr.olasz@gmail.com>
2021-12-13 23:01:13 -0800

    correcting issue with tag get requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,9 +6,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: {
-        model: Product,
-      },
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -20,7 +17,6 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
     });
     if (!tagData) {
       res.status(404).json({message: "NO DATA FOR THIS ID 🧁✨🐱‍👤" });
